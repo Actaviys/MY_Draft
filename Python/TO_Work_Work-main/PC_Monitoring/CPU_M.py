815887ef4a79e530b8edae5379f603392ea5fdc6
Actaviys <actaviys88@gmail.com>
2025-01-07 16:41:22 +0200

    W

--- Python/TO_Work_Work-main/PC_Monitoring/CPU_M.py
+++ Python/TO_Work_Work-main/PC_Monitoring/CPU_M.py
@@ -1,25 +1,26 @@
-import psutil as pl
-
-
-# print(pl.cpu_count()) # Кількість віртуальних ядер процесора (CPU)
-# print(pl.cpu_freq()) # Загальна частота процесора (CPU)
-# print(pl.cpu_percent(interval=1)) # Навантаження на процесор (CPU)
-# print(pl.cpu_times_percent(interval=0.9)) # Процентний стан процесора з інтервалом (CPU)
-# print(pl.cpu_stats()) # Статус процесів (CPU)
-# print(pl.cpu_times()) # Час працювання (CPU)
-
-
-# print(pl.disk_partitions()) # Список дисків на ПК
-# print(pl.disk_usage("/")) # Навантаження на диск
+import psutil_EY as plM
 
 
 
-# print(pl.Process().username()) # Ім'я комп'ютера
-# print(pl.Process().cmdline()) # Виводить посилання cmd на файл з відки запушенно файл
-# print(pl.Process().cpu_affinity()) # Список ядер процесора
-# print(pl.boot_time()) # Виводить час роботи процесора
+# print(plM.cpu_count()) # Кількість віртуальних ядер процесора (CPU)
+# print(plM.cpu_freq()) # Загальна частота процесора (CPU)
+# print(plM.cpu_percent(interval=1)) # Навантаження на процесор (CPU)
+# print(plM.cpu_times_percent(interval=0.9)) # Процентний стан процесора з інтервалом (CPU)
+# print(plM.cpu_stats()) # Статус процесів (CPU)
+# print(plM.cpu_times()) # Час працювання (CPU)
+
+
+# print(plM.disk_partitions()) # Список дисків на ПК
+# print(plM.disk_usage("/")) # Навантаження на диск
+
+
+
+# print(plM.Process().username()) # Ім'я комп'ютера
+# print(plM.Process().cmdline()) # Виводить посилання cmd на файл з відки запушенно файл
+# print(plM.Process().cpu_affinity()) # Список ядер процесора
+# print(plM.boot_time()) # Виводить час роботи процесора
 #
 
 
 while True:
-    print(pl.cpu_percent(interval=1.5))
+    print(plM.cpu_percent(interval=1.5))

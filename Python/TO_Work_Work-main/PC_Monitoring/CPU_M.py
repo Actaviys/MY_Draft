54247004d9216f430ccedce8d9a87839abdd2965
Actaviys <actaviys88@gmail.com>
2025-01-07 18:28:06 +0200

    W

--- Python/TO_Work_Work-main/PC_Monitoring/CPU_M.py
+++ Python/TO_Work_Work-main/PC_Monitoring/CPU_M.py
@@ -1,6 +1,5 @@
-import psutil_EY as plM
-
-
+## 1
+# import psutil_EY as plM
 
 # print(plM.cpu_count()) # Кількість віртуальних ядер процесора (CPU)
 # print(plM.cpu_freq()) # Загальна частота процесора (CPU)
@@ -14,13 +13,25 @@ import psutil_EY as plM
 # print(plM.disk_usage("/")) # Навантаження на диск
 
 
-
 # print(plM.Process().username()) # Ім'я комп'ютера
 # print(plM.Process().cmdline()) # Виводить посилання cmd на файл з відки запушенно файл
 # print(plM.Process().cpu_affinity()) # Список ядер процесора
 # print(plM.boot_time()) # Виводить час роботи процесора
-#
 
 
-while True:
-    print(plM.cpu_percent(interval=1.5))
+# while True:
+#     print(plM.cpu_percent(interval=1.5))
+
+
+
+
+## 2
+import platform as pf
+
+pc = pf.uname()
+# print(pc.system) # Інформація про OS
+# print(pc.node) # Назва ПК
+# print(pc.machine) # Назва процесора
+# print(pc.release) # Реліз
+# print(pc.processor) # Опис процесора
+print(pc.version)
